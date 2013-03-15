007f9e6176a7a3673b65f9e5778b08681907683c
Noel Delgado <auhcsei@gmail.com>
2013-03-14 23:52:27 -0600

    simplifying dropdown

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,6 +7,11 @@ var Viewport = (function(){
         _reload     = document.getElementById('reload'),
         _notify     = document.getElementById('notify');
 
+    _dropdown.element   = document.getElementById('dropdead');
+    _dropdown.current   = _dropdown.element.querySelector('button');
+    _dropdown.list      = _dropdown.element.querySelector('select');
+    _dropdown.items;
+
     var presets = {
         "small-phone"   : {w: 240, h: 320,  desc: 'Small Phone'},
         "iphone"        : {w: 320, h: 480,  desc: 'iPhone'},
@@ -15,16 +20,10 @@ var Viewport = (function(){
     };
     var defaultPreset = presets["iphone"];
 
-    _dropdown.element   = document.getElementById('dropdead');
-    _dropdown.current   = _dropdown.element.querySelector('button');
-    _dropdown.list      = _dropdown.element.querySelector('ul');
-    _dropdown.items;
-
     var init = function() {
         createDropdown();
-        _dropdown.items = _dropdown.element.querySelectorAll('li');
-        setCurrent(defaultPreset.w, defaultPreset.h, defaultPreset.desc);
-        _dropdown.items[1].className = "active"; // :(
+        updateDropdown(defaultPreset.w, defaultPreset.h, defaultPreset.desc);
+        _dropdown.list[1].selected = true; // :(
         setSize(defaultPreset.w, defaultPreset.h);
         setURL(this.url);
         bindEvents();
@@ -34,46 +33,27 @@ var Viewport = (function(){
         _url.addEventListener('click', getUrl, false);
         _reload.addEventListener('click', reload, false);
 
-        // _iframe.addEventListener('load', function() {}, false);
-
-        _dropdown.current.addEventListener('click', function(event) {
-            if (_dropdown.list.style.display != 'none') {
-                _dropdown.list.style.display = 'none';
-                _dropdown.current.classList.remove("active");
-            } else {
-                _dropdown.list.style.display = '';
-                _dropdown.current.className += " active";
-            }
+        _dropdown.list.addEventListener('mousedown', function(event) {
+            _dropdown.current.classList.add("active");
         }, false);
 
-        _dropdown.list.addEventListener('click', function(event) {
-            if (event.target && event.target.nodeName == "LI") {
-                var w = event.target.dataset.width,
-                    h = event.target.dataset.height,
-                    d = event.target.dataset.desc;
-
-                for (var i in _dropdown.items) {
-                    _dropdown.items[i].className = "";
-                }
-
-                event.target.className = "active";
-                setCurrent(w, h, d);
-                setSize(w, h);
-            }
+        _dropdown.list.addEventListener('blur', function(event) {
+            _dropdown.current.classList.remove("active");
         }, false);
 
-        document.getElementsByTagName('body')[0].addEventListener('click', function(event) {
-            if (event.target && event.target.nodeName === "BODY" || event.target.nodeName === "HEADER") {
-                _dropdown.list.style.display = "none";
-                _dropdown.current.classList.remove("active");
-            }
-        }, false);
+        _dropdown.list.addEventListener('change', function(event) {
+            var w = this.selectedOptions[0].dataset.width,
+                h = this.selectedOptions[0].dataset.height,
+                d = this.selectedOptions[0].dataset.desc;
+            setSize(w, h);
+            updateDropdown(w, h, d);
+        });
     };
 
     var createDropdown = function() {
         var frag = document.createDocumentFragment();
         for (var i in presets) {
-            var item    = document.createElement('li'),
+            var item    = document.createElement('option'),
                 w       = presets[i].w,
                 h       = presets[i].h,
                 d       = presets[i].desc;
@@ -84,31 +64,38 @@ var Viewport = (function(){
             item.setAttribute('data-desc', d);
             frag.appendChild(item);
         }
-        _dropdown.list.style.display = "none";
         _dropdown.list.appendChild(frag);
     };
 
-    var setCurrent = function(w, h, d) {
+    var updateDropdown = function(w, h, d) {
         _dropdown.current.classList.remove("active");
-        _dropdown.list.style.display = "none";
         _dropdown.current.innerHTML = w + " x " + h + " - " + d;
     };
 
     var setSize = function(w, h) {
         _target.style.width     = w + 'px';
         _target.style.height    = h + 'px';
+        return this;
     };
 
     var setURL = function(url) {
         _iframe.src = url;
+        return this;
+    };
+
+    var reload = function() {
+        _iframe.src = _iframe.src;
+        return this;
     };
 
     var getUrl = function() {
         var pattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
-            url = prompt("Enter URL:");
-        pattern.test(url)
-            ? setURL(url)
-            : notify("Invalid URL");
+            url     = prompt("Enter URL:");
+        if (url !== null) {
+            pattern.test(url)
+                ? setURL(url)
+                : notify("Invalid URL");
+        }
     };
 
     var notify = function(message) {
@@ -119,12 +106,8 @@ var Viewport = (function(){
         }, 2000);
     };
 
-    var reload = function() {
-        _iframe.src = _iframe.src;
-    };
-
     return  {
-        url: "http://pixelia.me",
+        url: "http://git.local/js/01-public/text-to-binary/",
         init    : init,
         setSize : setSize,
         setURL  : setURL,
